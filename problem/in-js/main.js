11e2a6e6057fa5ba91e4103c6b8d3b99cf8bbeaa
Tanay Pratap <tanaypratap@Tanays-MacBook-Pro.local>
2018-11-17 12:06:22 +0530

    requestIdleCallback

--- problem/in-js/main.js
+++ problem/in-js/main.js
@@ -1,16 +1,21 @@
-class TreeNode {
-    constructor(value, left, right) {
-        this.value = value;
-        this.left = left;
-        this.right = right;
-    }
-}
+window.requestIdleCallback =  window.requestIdleCallback || function (handler) {
+    let startTime = Date.now();
+
+    return setTimeout(function () {
+        handler({
+            didTimeout: false,
+            timeRemaining: function () {
+                return Math.max(0, 50.0 - (Date.now() - startTime));
+            }
+        });
+    }, 1);
+};
 
 class QNode {
     constructor(val, next) {
         this.val = val;
         this.next = next;
-    } 
+    }
 }
 
 class Queue {
@@ -21,96 +26,94 @@ class Queue {
 
     add(val) {
         let tmp = new QNode(val, null);
-        if(this.root == null) {
+
+        if (this.root == null) {
             this.root = tmp;
             this.end = tmp;
         } else {
             this.end.next = tmp;
             this.end = tmp;
-        }        
-    }   
-
-    delete() {
-        let tmp = this.root;
-        this.root = this.root.next;
-        return tmp;
-    }
-
-    isEmpty() {
-        if(this.root == null) return true;
-        return false;
+        }
     }
 }
 
-const SIZE = 3000;
+const SIZE = 5000;
 
-function buildTree(size) {
-    let queue =new Queue();
-    let root = new TreeNode(Math.floor(Math.random() * 100), null, null);
-    queue.add(root);
-    size--;
-    while(!queue.isEmpty()) {
-        let top = queue.delete();
-        if(size > 0) {
-            let tmp = new TreeNode(Math.floor(Math.random() * 100), null, null);
-            top.val.left = tmp;
-            queue.add(tmp);
-            size--;
-        }
-        if(size > 0) {
-            let tmp = new TreeNode(Math.floor(Math.random() * 100), null, null);
-            top.val.right = tmp;
-            queue.add(tmp);
-            size--;
-        }
-    } 
-    return root;
+function createQueue(size) {
+    let queue = new Queue();
+
+    while(size > 0) {
+        queue.add(Math.floor(Math.random() * 100));
+        size--;
+    }
+
+    console.log(queue)
+    return queue;
+
 }
 
-const root = buildTree(SIZE);
+let root = createQueue(SIZE).root;
 
 function delay() {
-    var e = performance.now() + 2;
-    while (performance.now() < e) {
-        // Artificially long execution time.
-    }
+  var e = performance.now() + 2;
+  while (performance.now() < e) {
+    // Artificially long execution time.
+  }
 }
 
 function recursiveSum(cur) {
     if (cur == null) {
         return 0;
     }
+
     delay()
-    return cur.value + recursiveSum(cur.left) +  recursiveSum(cur.right);
+    return cur.val + recursiveSum(cur.next);
 }
 
 function recursiveHandler() {
-    var sum = recursiveSum(root);
-    console.log(sum);
+  var sum = recursiveSum(root);
+  console.log('Recursive Summation Done...', sum);
 }
 
-function traverseLinear(cur, lim) {
-    let result = 0;
-    for(let i = 0; i < lim; i++) {
-        if(cur == null) break;
-        result += cur.value;
-        cur = cur.next;
+
+
+/**
+ * One unit of work
+ */
+function traverseLinear(sum, cur) {
+    sum = sum + cur.val;
+    cur = cur.next;
+    delay();
+    return { cur, sum }
+}
+
+/**
+ * Checking before every unit of work
+ */
+const traverseTree = (sum, cur) => (deadline) => {
+    while (deadline.timeRemaining() > 0  && cur !== null) {
+        const workCompleted = traverseLinear(sum, cur);
+        sum = workCompleted.sum;
+        cur = workCompleted.cur;
     }
-    return {cur, result};
+
+    if (cur !== null) { // more work to do
+        requestIdleCallback(traverseTree(sum, cur))
+    }
+
+    if (cur === null) {
+        console.log('Linear Summation Done...', sum)
+    }
+
 }
 
 
-
+/**
+ * Click Handler
+ */
 function linearHandler() {
     let sum = 0, cur = root;
-    // for(let i = 0; i < 10; i++) {
-    //     setTimeout(() => {
-    //         let res = traverseLinear(cur, SIZE / 10);
-    //         sum += res.result;
-    //         cur = res.cur;
-    //     }, 0)
-    // }
-    // setTimeout(() => console.log(sum), 0);
+    requestIdleCallback(traverseTree(sum, cur));
 
 
 }
